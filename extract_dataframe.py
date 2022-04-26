467f4b770c309de3076ec0f7a2ca0f89926648fa
Ammon21 <ammonb6@gmail.com>
2022-04-26 19:40:28 +0300

    file succesfuly saved!

--- extract_dataframe.py
+++ extract_dataframe.py
@@ -163,8 +163,7 @@ if __name__ == "__main__":
     'original_author', 'screen_count', 'followers_count','friends_count','possibly_sensitive', 'hashtags', 'user_mentions', 'place', 'place_coord_boundaries']
     _, tweet_list = read_json(r"C:\Users\dell\Desktop\Twitter-data-Analysis\data\Economic_Twitter_Data.json")
     tweet = TweetDfExtractor(tweet_list)
-    tweet_df = tweet.get_tweet_df() 
-    print(tweet_df.head())
+    tweet_df = tweet.get_tweet_df(True) 
     # use all defined functions to generate a dataframe with the specified columns above
 
     
